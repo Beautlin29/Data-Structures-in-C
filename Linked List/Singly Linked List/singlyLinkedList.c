6523416bfa54c83d32f908380040d38b0072f594
Beautlin S <beautlin87@gmail.com>
2023-05-26 19:11:07 +0530

    Create singlyLinkedList.c

---
+++ Linked List/Singly Linked List/singlyLinkedList.c
@@ -0,0 +1,48 @@
+//implementation of singly linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    struct node
+    {
+       int data;
+       struct node *next;
+    };
+
+    struct node *head = 0, *newNode, *temp;
+    int choice = 1;
+
+    while(choice)
+    {
+        newNode = (struct node*)(malloc(sizeof(struct node)));
+    
+
+    printf("Enter the number: ");
+    scanf("%d", &newNode->data);
+    newNode->next = NULL;
+
+    if(head == 0)
+    {
+        head = temp = newNode;
+    }
+    else
+    {
+        temp->next = newNode;
+        temp = temp->next;
+    }
+
+    printf("Do you want to continue(0,1): ");
+    scanf("%d", &choice);
+    }
+
+    temp = head;
+    printf("Linked List: ");
+    while (temp != 0)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }   
+    
+}
