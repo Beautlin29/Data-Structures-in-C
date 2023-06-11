f70e327d3edbe2e72dbd15f8420509fd011bd9a7
Beautlin S <beautlin87@gmail.com>
2023-06-11 11:11:08 +0530

    implemented binary tree

---
+++ Binary Tree/binaryTreeImplementation.c
@@ -0,0 +1,33 @@
+// implementation of binary tree using linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left, *right;
+} *root = NULL, *newNode;
+
+struct node *create()
+{
+    int num;
+    newNode = (struct node*)malloc(sizeof(struct node));
+    printf("\nEnter the data(-1 for no node): ");
+    scanf("%d", &num);
+    if(num == -1)
+    {
+        return 0;
+    }
+    newNode->data = num;
+    printf("Left data: ");
+    newNode->left = create();
+    printf("Right data: ");
+    newNode->right = create();
+    return newNode;
+}
+
+void main()
+{
+    root = create();
+}
