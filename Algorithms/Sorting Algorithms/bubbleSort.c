419f6e44c72bdbe6592ab7f885e826da65cd0572
Beautlin S <beautlin87@gmail.com>
2023-06-13 22:24:42 +0530

    added swap function

--- Algorithms/Sorting Algorithms/bubbleSort.c
+++ Algorithms/Sorting Algorithms/bubbleSort.c
@@ -5,9 +5,18 @@
 
 #define N 10
 
+// swap 
+
+void swap(int *a, int *b) 
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void main()
 {
-    int array[N] = {59, 23, 55, 11, 76, 56, 44, 99, 47, 22}, temp = 0, swap = 0;
+    int array[N] = {59, 23, 55, 11, 76, 56, 44, 99, 47, 22}, flag = 0;
 
     printf("Unsorted array: ");
     for(int i = 0; i < N; i++)
@@ -17,19 +26,17 @@ void main()
 
     for(int i = 0; i < N-1; i++)
     {
-        swap = 0;
+        flag = 0;
 
         for(int j = 0; j < N-i-1; j++)
         {
             if(array[j] > array[j+1])
             {
-                temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-                swap = 1;
+                swap(&array[j], &array[j+1]);
+                flag = 1;
             }
         }
-        if(swap == 0)
+        if(flag == 0)
         {
             break;  
         }
